a55967a9dad88faea6d9cc70efd0518d4d05ccd9
sunxyw <git@sunxyw.xyz>
2023-02-14 02:42:34 +0800

    add asset publish command spawner

--- src/AssetPublisher.php
+++ src/AssetPublisher.php
@@ -6,6 +6,7 @@ namespace Sunxyw\AssetPublisher;
 
 use ZM\Annotation\Framework\Setup;
 use ZM\Plugin\PluginMeta;
+use ZM\Store\FileSystem;
 
 class AssetPublisher
 {
@@ -17,11 +18,20 @@ class AssetPublisher
 
     public function spawnPublishCommand(): void
     {
-        logger()->info('spawnPublishCommand is running');
+        $this->copyFile(__DIR__ . '/Commands/AssetPublishCommand.php', 'src/Commands/AssetPublishCommand.php');
     }
 
     public function publish(PluginMeta $plugin, string $type): void
     {
         dump($plugin, $type);
+    }
+
+    private function copyFile(string $source, string $target): void
+    {
+        // target is based on SOURCE_ROOT_DIR
+        $target = SOURCE_ROOT_DIR . '/' . $target;
+        FileSystem::createDir(dirname($target));
+        FileSystem::ensureFileWritable($target);
+        copy($source, $target);
     }
 }
